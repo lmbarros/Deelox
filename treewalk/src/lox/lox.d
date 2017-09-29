b65699cf1233c68fbad247f97609ca4ef5202055
Leandro Motta Barros <lmb@stackedboxes.org>
2017-09-29 20:37:00 -0300

    Initial commit

---
+++ treewalk/src/lox/lox.d
@@ -0,0 +1,20 @@
+//
+// Deelox: Lox interpreter in D
+//
+// Leandro Motta Barros (just adapting the code from Bob Nystrom's Book, see
+// http://www.craftinginterpreters.com)
+//
+module lox.lox;
+
+public class Lox
+{
+    void runFile(const string file)
+    {
+        // ...
+    }
+
+    void runPrompt()
+    {
+        // ...
+    }
+}
---
+++ treewalk/src/main.d
@@ -0,0 +1,31 @@
+//
+// Deelox: Lox interpreter in D
+//
+// Leandro Motta Barros (just adapting the code from Bob Nystrom's Book, see
+// http://www.craftinginterpreters.com)
+//
+
+
+import lox.lox;
+
+void main(string[] args)
+{
+    import std.stdio: writeln;
+
+    writeln("This is deelox, treewalk version");
+
+    auto lox = new Lox();
+
+    if (args.length > 2)
+    {
+        writeln("Usage: deelox <script>");
+    }
+    else if (args.length == 2)
+    {
+        lox.runFile(args[0]);
+    }
+    else
+    {
+        lox.runPrompt();
+    }
+}
