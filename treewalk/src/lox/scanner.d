b95fe9c0d9c81b0f98e0db54d37b62a1e0d1faf1
Leandro Motta Barros <lmb@stackedboxes.org>
2017-09-30 12:04:15 -0300

    Done up to section 4.6.2
    http://www.craftinginterpreters.com/scanning.html

--- treewalk/src/lox/scanner.d
+++ treewalk/src/lox/scanner.d
@@ -52,7 +52,13 @@ struct Scanner
         addToken(type, null);
     }
 
-    private void addToken(TokenType type, Object literal)
+    private void addToken(TokenType type, string literal)
+    {
+        const text = _source[_start.._current];
+        _tokens ~= Token(type, text, literal, _line);
+    }
+
+    private void addToken(TokenType type, double literal)
     {
         const text = _source[_start.._current];
         _tokens ~= Token(type, text, literal, _line);
@@ -79,10 +85,92 @@ struct Scanner
             case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
             case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
             case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+            case '/':
+                if (match('/'))
+                {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                }
+                else
+                {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ': case '\r': case '\t':
+                // Ignore whitespace.
+                break;
+
+            case '\n':
+                ++_line;
+                break;
+
+            case '"':
+                scanString();
+                break;
+
+            case '0': .. case '9':
+                scanNumber();
+                break;
+
             default: Lox.error(_line, "Unexpected character: '" ~ c ~ "'");
         }
     }
 
+    private bool isDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    private void scanNumber()
+    {
+        import std.conv: to;
+
+        while (isDigit(peek()))
+            advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext()))
+        {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(TokenType.NUMBER, to!double(_source[_start.._current]));
+    }
+
+    private void scanString()
+    {
+        import lox.lox: Lox;
+
+        while (peek() != '"' && !isAtEnd())
+        {
+            if (peek() == '\n')
+                ++_line;
+            advance();
+        }
+
+        // Unterminated string.
+        if (isAtEnd())
+        {
+            Lox.error(_line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes. This would be the point to unescape
+        // characters, if escaped characters were suported.
+        const value = _source[_start + 1 .. _current - 1];
+        addToken(TokenType.STRING, value);
+    }
+
+    // Consumes the current character.
     private bool match(char expected)
     {
         if (isAtEnd())
@@ -93,5 +181,22 @@ struct Scanner
 
         _current++;
         return true;
+    }
+
+    // Doesn't consume the current character. Just plain lookahead.
+    private char peek()
+    {
+        if (isAtEnd())
+            return '\0';
+
+        return _source[_current];
+    }
+
+    // Look two characters ahead.
+    private char peekNext()
+    {
+        if (_current + 1 >= _source.length)
+            return '\0';
+        return _source[_current + 1];
     }
 }
--- treewalk/src/lox/token.d
+++ treewalk/src/lox/token.d
@@ -9,22 +9,51 @@ module lox.token;
 
 import lox.token_type: TokenType;
 
-struct Token
+public union Literal
+{
+    string str;
+    double number;
+}
+
+
+public struct Token
 {
     public TokenType type;
     public string lexeme;
-    public Object literal; // TODO: I guess this will not work in D
+    public Literal literal;
     public int line;
 
-    public this(TokenType type, string lexeme, Object literal, int line) {
+    public this(TokenType type, string lexeme, string literal, int line)
+    {
         this.type = type;
         this.lexeme = lexeme;
-        this.literal = literal;
+        this.literal.str = literal;
         this.line = line;
     }
 
-  public string toString() {
-      import std.conv: to;
-      return to!string(type) ~ " '" ~ lexeme ~ "' '" ~ to!string(literal) ~ "'";
-  }
+    public this(TokenType type, string lexeme, double literal, int line)
+    {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal.number = literal;
+        this.line = line;
+    }
+
+    public string toString()
+    {
+        import std.conv: to;
+        auto lit = "";
+
+        with (TokenType) switch(type)
+        {
+            case STRING:
+                lit = literal.str; break;
+            case NUMBER:
+                lit = to!string(literal.number); break;
+            default:
+                break;
+        }
+
+        return to!string(type) ~ " '" ~ lexeme ~ "' [" ~ lit ~ "]";
+    }
 }
