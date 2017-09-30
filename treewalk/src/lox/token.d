a39fcd19ae69e2e07c638c6b65cef0a534c6ace1
Leandro Motta Barros <lmb@stackedboxes.org>
2017-09-29 21:38:51 -0300

    Done up to section 4.2 (Token, TokenType)
    http://www.craftinginterpreters.com/scanning.html

---
+++ treewalk/src/lox/token.d
@@ -0,0 +1,30 @@
+//
+// Deelox: Lox interpreter in D
+//
+// Leandro Motta Barros (just adapting the code from Bob Nystrom's Book, see
+// http://www.craftinginterpreters.com)
+//
+
+module lox.token;
+
+import lox.token_type: TokenType;
+
+struct Token
+{
+    public TokenType type;
+    public string lexeme;
+    public Object literal; // TODO: I guess this will not work in D
+    public int line;
+
+    public this(TokenType type, string lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+  public string toString() {
+      import std.conv: to;
+      return to!string(type) ~ " " ~ lexeme ~ " " ~ to!string(literal);
+  }
+}
---
+++ treewalk/src/lox/token_type.d
@@ -0,0 +1,31 @@
+//
+// Deelox: Lox interpreter in D
+//
+// Leandro Motta Barros (just adapting the code from Bob Nystrom's Book, see
+// http://www.craftinginterpreters.com)
+//
+
+module lox.token_type;
+
+enum TokenType
+{
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  // End of file
+  EOF
+}
