b1cf104ff0f6caeda3874605bcc2f2756ae5aca6
Leandro Motta Barros <lmb@stackedboxes.org>
2017-09-29 21:08:35 -0300

    Done up to session 4.1
    http://www.craftinginterpreters.com/scanning.html

--- treewalk/src/lox/lox.d
+++ treewalk/src/lox/lox.d
@@ -8,13 +8,63 @@ module lox.lox;
 
 public class Lox
 {
-    void runFile(const string file)
+    public void runFile(const string file)
     {
-        // ...
+        import core.stdc.stdlib: exit;
+        import std.file: readText;
+
+        auto source = readText(file);
+        run(source);
+
+        // Indicate an error in the exit code.
+        if (_hadError)
+            exit(65);
     }
 
-    void runPrompt()
+    public void runPrompt()
     {
-        // ...
+        import std.stdio: readln, write;
+
+        while(true)
+        {
+            write("> ");
+            run(readln());
+            _hadError = false;
+        }
     }
+
+    private void run(string source)
+    {
+        import std.stdio: writeln;
+        writeln("Running (well, not yet) this source:\n-----------------\n",
+            source, "-----------------");
+
+        /+
+        import lex.scanner: Scanner;
+        import lex.token: Token;
+
+        auto  scanner = new Scanner(source);
+        Token[] tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        foreach (token; tokens)
+        {
+            writeln(token);
+        }
+        +/
+    }
+
+    private void error(int line, string message)
+    {
+        report(line, "", message);
+    }
+
+    private void report(int line, string where, string message)
+    {
+        import std.stdio: writefln;
+        writefln("[line %s] Error: %s: %s", line, where, message);
+        _hadError = true;
+    }
+
+    private bool _hadError = false;
 }
